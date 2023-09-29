95c987a70d321479ab6e51dace509677592413de
Steffan Burnette <burnettesteffansep7@gmail.com>
2023-09-28 22:09:07 -0400

    New

--- mergesort.js
+++ mergesort.js
@@ -24,7 +24,7 @@ function mergeSorted(ary1, ary2) {
         } else {
             sortedAry.push(ary2[j]);
             j++;     
-        }
+        }         
     }         
 
     // If there are  remaining elements in ary1, add them to sortedAry
